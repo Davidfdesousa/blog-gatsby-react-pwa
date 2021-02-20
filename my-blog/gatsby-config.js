61b6e1055c8a97ff9016243de2f9a68642ae1dc6
David Ferreira <david.ferreira@superare.co>
2021-02-20 00:37:59 -0300

    cleanup starter stuff

--- my-blog/gatsby-config.js
+++ my-blog/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `My blog`,
+    description: `A blog about frontend development and other cool stuff.`,
     author: `@gatsbyjs`,
   },
   plugins: [
